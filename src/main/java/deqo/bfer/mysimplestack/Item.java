0e2460c6b85ad41a8a61cd012163c9535a1428e9
BastienFernandez <Fernandez.bastien.mail@gmail.com>
2018-10-25 10:41:50 +0200

    ajout de methodes dans item

--- src/main/java/deqo/bfer/mysimplestack/Item.java
+++ src/main/java/deqo/bfer/mysimplestack/Item.java
@@ -3,11 +3,19 @@ package deqo.bfer.mysimplestack;
 public class Item {
     private int value;
 
+    public Item () {
+        this.value = 0;
+    }
+
     public Item (int value) {
         this.value = value;
     }
 
     public int getValue () {
         return value;
+    }
+
+    public void setValue (int value) {
+        this.value = value;
     }
 }
